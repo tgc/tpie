48b54ff3c86d94896718888544ccd43bda8c7084
Jakob Truelsen <jakob@scalgo.com>
2010-09-17 15:42:38 +0200

    include utility for pair in unused

--- tpie/unused.h
+++ tpie/unused.h
@@ -1,6 +1,6 @@
 // -*- mode: c++; tab-width: 4; indent-tabs-mode: t; eval: (progn (c-set-style "stroustrup") (c-set-offset 'innamespace 0)); -*-
 // vi:set ts=4 sts=4 sw=4 noet :
-// Copyright 2008, The TPIE development team
+// Copyright 2010, The TPIE development team
 // 
 // This file is part of TPIE.
 // 
@@ -19,6 +19,7 @@
 #ifndef __TPIE_UNUSED_H__
 #define __TPIE_UNUSED_H__
 #include <limits>
+#include <utility>
 namespace tpie {
 
 template <typename T>
