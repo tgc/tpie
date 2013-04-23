f90f0b86877494b9476d9384ddead1fd3ab64760
Mathias Rav <rav@cs.au.dk>
2013-04-23 12:50:45 +0200

    b_tree.h: Introduce {node,leaf}{Min,Max} instead of `a` and `b`.

--- tpie/blocks/b_tree.h
+++ tpie/blocks/b_tree.h
@@ -60,8 +60,10 @@ struct b_tree_header {
 };
 
 struct b_tree_parameters {
-	uint64_t a;
-	uint64_t b;
+	uint64_t nodeMin;
+	uint64_t nodeMax;
+	uint64_t leafMin;
+	uint64_t leafMax;
 };
 
 template <typename Key>
@@ -126,6 +128,11 @@ class b_tree_leaf {
 	typedef typename Traits::Compare Compare;
 
 public:
+	static memory_size_type calculate_fanout(memory_size_type blockSize) {
+		blockSize -= sizeof(b_tree_header);
+		return blockSize / sizeof(Value); // floored division
+	}
+
 	b_tree_leaf(block_buffer & buffer, const b_tree_parameters & params)
 		: m_params(params)
 	{
@@ -150,13 +157,14 @@ public:
 	}
 
 	// Definition 1, first bullet:
-	// All leaves of T are on the same level and contain between a and b elements.
+	// All leaves of T are on the same level and contain
+	// between leafMin and leafMax elements.
 	bool full() const {
-		return m_header->degree == m_params.b;
+		return m_header->degree == m_params.leafMax;
 	}
 
 	bool underfull() const {
-		return m_header->degree < m_params.a;
+		return m_header->degree < m_params.leafMin;
 	}
 
 	void insert(Value v) {
@@ -167,13 +175,13 @@ public:
 
 	// Returns the minimum key in the right buffer.
 	Key split_insert(Value v, block_buffer & rightBuf, const Compare & comp) {
-		if (m_header->degree != m_params.b) throw exception("Split insert in non-full leaf");
+		if (m_header->degree != m_params.leafMax) throw exception("Split insert in non-full leaf");
 
 		b_tree_leaf<Traits> rightLeaf(rightBuf, m_params);
 
-		Value * endPoint = m_values + m_params.b;
+		Value * endPoint = m_values + m_params.leafMax;
 		Value * insertionPoint = std::partition(m_values, endPoint, key_less_than<Traits>(comp, v));
-		Value * splitPoint = m_values + m_params.b/2;
+		Value * splitPoint = m_values + m_params.leafMax/2;
 
 		// All values in [m_values, insertionPoint) are less than v,
 		// which is less than all values in [insertionPoint, endPoint).
@@ -260,7 +268,7 @@ public:
 		: m_params(params)
 	{
 		char * children = buffer.get() + sizeof(b_tree_header);
-		char * keys = children + params.b * sizeof(block_handle);
+		char * keys = children + params.nodeMax * sizeof(block_handle);
 
 		m_header = reinterpret_cast<b_tree_header *>(buffer.get());
 		m_children = reinterpret_cast<block_handle *>(children);
@@ -283,14 +291,15 @@ public:
 	}
 
 	// Definition 1, second bullet:
-	// Except for the root, all nodes have degree between a and b (contain
-	// between a - 1 and b - 1 elements)
+	// Except for the root, all nodes have degree
+	// between nodeMin and nodeMax
+	// (contain between nodeMin - 1 and nodeMax - 1 elements)
 	bool full() const {
-		return degree() == m_params.b;
+		return degree() == m_params.nodeMax;
 	}
 
 	bool underfull() const {
-		return degree() < m_params.a;
+		return degree() < m_params.nodeMin;
 	}
 
 	Key key(memory_size_type idx) const {
@@ -417,7 +426,7 @@ public:
 			children[output] = right.child(right.keys());
 		}
 
-		if (children.size() <= m_params.b) {
+		if (children.size() <= m_params.nodeMax) {
 			std::copy(keys.begin(), keys.end(), &left.m_keys[0]);
 			std::copy(children.begin(), children.end(), &left.m_children[0]);
 			left.m_header->degree = static_cast<uint64_t>(children.size());
@@ -505,10 +514,18 @@ public:
 		, m_treeHeight(0)
 	{
 		open();
-		m_params.b = b_tree_block<Traits>::calculate_fanout(block_size());
-		m_params.a = (m_params.b + 3)/4;
-		if (m_params.a < 2) throw exception("Block size too small; a >= 2 violated");
-		if (m_params.b < m_params.a*2-1) throw exception("Block size too small; b >= 2a-1 violated");
+		m_params.nodeMax = b_tree_block<Traits>::calculate_fanout(block_size());
+		m_params.nodeMin = (m_params.nodeMax + 3)/4;
+		m_params.leafMax = b_tree_leaf<Traits>::calculate_fanout(block_size());
+		m_params.leafMin = (m_params.leafMax + 3)/4;
+		if (m_params.nodeMin < 2)
+			throw exception("Block size too small; nodeMin >= 2 violated");
+		if (m_params.nodeMax < m_params.nodeMin*2-1)
+			throw exception("Block size too small; nodeMax >= 2a-1 violated");
+		if (m_params.leafMin < 2)
+			throw exception("Block size too small; leafMin >= 2 violated");
+		if (m_params.leafMax < m_params.leafMin*2-1)
+			throw exception("Block size too small; leafMax >= 2a-1 violated");
 	}
 
 	~b_tree() {
